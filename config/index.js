9a486270a56243eecc8edeacdaf28993396553e1
Robert <464554492@qq.com>
2018-08-27 10:08:33 +0800

    更改service结构,去掉api

--- config/index.js
+++ config/index.js
@@ -22,7 +22,13 @@ module.exports = {
         // Paths
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
-        proxyTable: {},
+        proxyTable: {
+            '/proxy': {
+                target: 'http://127.0.0.1:3000',
+                changeOrigin: true,
+                pathRewrite: {'^/proxy': ''}
+            }
+        },
 
         // Various Dev Server settings
         host: 'localhost', // can be overwritten by process.env.HOST
